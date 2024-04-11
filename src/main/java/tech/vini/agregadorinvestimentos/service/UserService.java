e08663be99d5f79c3052c1dfd9d46bc1080c0303
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-04-11 17:26:45 -0300

    feat: create User service

---
+++ src/main/java/tech/vini/agregadorinvestimentos/service/UserService.java
@@ -0,0 +1,32 @@
+package tech.vini.agregadorinvestimentos.service;
+
+import org.springframework.stereotype.Service;
+import tech.vini.agregadorinvestimentos.dto.user.CreateUserDto;
+import tech.vini.agregadorinvestimentos.entity.User;
+import tech.vini.agregadorinvestimentos.repository.UserRepository;
+
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto){
+        var entity = new User(
+                UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                null,
+                null);
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+}
