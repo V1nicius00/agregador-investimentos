1e616a8aa3b173a7f6bfbced1c8010bf58a1cc72
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-04-12 16:52:20 -0300

    feat: create UserServiceTest

---
+++ src/test/java/tech/vini/agregadorinvestimentos/service/UserServiceTest.java
@@ -0,0 +1,19 @@
+package tech.vini.agregadorinvestimentos.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tech.vini.agregadorinvestimentos.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+}
