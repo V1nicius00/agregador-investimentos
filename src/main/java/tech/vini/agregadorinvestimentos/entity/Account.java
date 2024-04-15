a9322fd2c5b0c2b56f4be09556c840414c01dd18
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-04-15 20:15:51 -0300

    feat: create Account entity

---
+++ src/main/java/tech/vini/agregadorinvestimentos/entity/Account.java
@@ -0,0 +1,26 @@
+package tech.vini.agregadorinvestimentos.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String description;
+}
