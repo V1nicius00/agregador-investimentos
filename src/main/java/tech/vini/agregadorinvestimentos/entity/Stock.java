65f3771d20322732fb5553ffacd22f49797d968c
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-04-16 14:49:25 -0300

    feat: create Stock entity

---
+++ src/main/java/tech/vini/agregadorinvestimentos/entity/Stock.java
@@ -0,0 +1,26 @@
+package tech.vini.agregadorinvestimentos.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "stocks")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Stock {
+
+    @Id
+    @Column(name = "stock_id")
+    private String stockId;
+
+    @Column(name = "description")
+    private String description;
+}
